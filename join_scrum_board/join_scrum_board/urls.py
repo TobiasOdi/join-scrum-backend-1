051d517cee0c664d44d6fc63f46f091df9c270a3
Tobias Odermatt <odermatt.tobias.w@gmail.com>
2025-01-15 18:25:04 +0100

    commit

--- join_scrum_board/join_scrum_board/urls.py
+++ join_scrum_board/join_scrum_board/urls.py
@@ -16,8 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from board_app.api.views import DataView, SetCategoriesView
-from user_auth_app.api.views import TokenCheckView
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
